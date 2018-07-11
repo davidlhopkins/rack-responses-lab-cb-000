66d335a6f2bb2c977e4455a2de681643730be4cb
David Hopkins <dlhopkins2@gmail.com>
2018-07-11 03:32:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,16 +3,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-#    resp.write "#{num_1}\n"
-#    resp.write "#{num_2}\n"
-#    resp.write "#{num_3}\n"
-#
-#    if num_1==num_2 && num_2==num_3
-#      resp.write "You Win"
-#    else
-#      resp.write "You Lose"
-#    end
-#
+    if time < 12
+      resp.write "Morning"
+    else
+      resp.write "Afternoon"
+    end
+
     resp.finish
   end
 
